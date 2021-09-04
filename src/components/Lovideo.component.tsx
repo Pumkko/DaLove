76f1cece3c03abb9259dcfff7acc6de426523ae4
Puma <c.coque@hotmail.fr>
2021-09-04 18:42:07 +0200

    changes video ratio

--- src/components/Lovideo.component.tsx
+++ src/components/Lovideo.component.tsx
@@ -1,21 +1,36 @@
 import React from "react";
+import { View } from "react-native";
+import { Dimensions } from 'react-native';
 
 // @ts-ignore On peut ignorer l'erreur qui nous dit que react native video n'existe pas, c'est un faux positif
 import Video from 'react-native-video'
 
 export const Lovideo = ({ navigation }) => {
+  
+  
+  const width = Dimensions.get('screen').width;
+  const height = Dimensions.get('screen').height; 
+  
   return (
 
-    <Video
-      source={require('../assets/videos/julian.mp4')}
-      style={{ width: '100%', height: '100%' }}
-      controls={true}
-      onEnd={
-        () => {
-          navigation.navigate('MainView');
+    <View style={{ flex: 1, backgroundColor: 'black' }}>
+      <Video
+        source={require('../assets/videos/julian.mp4')}
+        resizeMode={'contain'}
+        style={
+          {
+            aspectRatio: width/height,
+            width: "100%"
+          }
         }
-      }
-    />
+        controls={true}
+        onEnd={
+          () => {
+              navigation.navigate('MainView');
+          }
+        }
+      />
+    </View>
   );
 }
 
