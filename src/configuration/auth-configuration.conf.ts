28bb7c11f75ca548e6101f2565eb111d9ede8b60
Puma <c.coque@hotmail.fr>
2021-09-19 08:53:24 +0200

    modify AuthConfiguration to add audience, and fix the compilation error the previous commit brought

--- src/configuration/auth-configuration.conf.ts
+++ src/configuration/auth-configuration.conf.ts
@@ -2,7 +2,12 @@
 
 import { Options } from 'react-native-auth0';
 
-export const AuthConfiguration: Options = {
+
+export interface Auth0Options extends Options {
+    audience: string
+}
+
+export const AuthConfiguration: Auth0Options = {
     clientId: '',
     domain: '',
     audience: ''
