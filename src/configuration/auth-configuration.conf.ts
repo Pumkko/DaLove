3d0b7d6b253f1fbecf6f8455c09e1ed4a49a4706
Puma <c.coque@hotmail.fr>
2021-09-20 15:53:06 +0200

    add react native config to project and production and development config

--- src/configuration/auth-configuration.conf.ts
+++ src/configuration/auth-configuration.conf.ts
@@ -1,14 +1,16 @@
 
 
 import { Options } from 'react-native-auth0';
+import Config from 'react-native-config';
 
 
 export interface Auth0Options extends Options {
     audience: string
 }
 
+
 export const AuthConfiguration: Auth0Options = {
-    clientId: '',
-    domain: '',
-    audience: ''
+    clientId: Config.AUTH0_CLIENT_ID,
+    domain: Config.AUTH0_DOMAIN,
+    audience: Config.AUTH0_AUDIENCE
 };
