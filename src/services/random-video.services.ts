3f28a5bf994a5fa6813936479df646ba58d16230
Puma <c.coque@hotmail.fr>
2021-09-20 11:55:45 +0200

    change temporary web source for RandomVideoService just so it's different from the fake on

--- src/services/random-video.services.ts
+++ src/services/random-video.services.ts
@@ -1,6 +1,6 @@
-import { injectable } from "inversify";
-import "react-native-get-random-values";
-import "react-native-url-polyfill/auto";
+import { injectable } from 'inversify';
+import 'react-native-get-random-values';
+import 'react-native-url-polyfill/auto';
 
 export interface IRandomVideoService {
   getRandomVideo(): Promise<any>;
@@ -11,18 +11,16 @@ export class FakeRandomVideoServiceFromAssets implements IRandomVideoService {
   private readonly fake_Video = require('../assets/mock-video/SampleVideo_720x480_1mb.mp4');
 
   getRandomVideo(): Promise<any> {
-    return Promise.resolve(this.fake_Video);
+      return Promise.resolve(this.fake_Video);
   }
 }
 
 @injectable()
 export class RandomVideoService implements IRandomVideoService {
-  constructor() {}
+    getRandomVideo(): Promise<any> {
 
-  getRandomVideo(): Promise<any> {
+        const source =  {uri: 'https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_640_3MG.mp4' };
 
-    const source =  {uri: 'https://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_10mb.mp4' }
-
-    return Promise.resolve(source);
-  }
+        return Promise.resolve(source);
+    }
 }
