04cb0d94aed49ca04d1dd1a8c510afcf18739921
Puma <c.coque@hotmail.fr>
2021-09-20 12:20:19 +0200

    add custom type for RandomVideoService

--- src/components/Lovideo.component.tsx
+++ src/components/Lovideo.component.tsx
@@ -1,14 +1,12 @@
 import React, { useEffect, useState } from 'react';
 
 import Video from 'react-native-video';
-import { FakeRandomVideoServiceFromAssets, IRandomVideoService, RandomVideoService } from '../services/random-video.services';
+import { IRandomVideoService, VideoSource } from '../services/random-video.services';
 import { Dimensions, View } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '../navigation/navigation-types';
-import { useContainer, useInjection } from 'inversify-react';
 import { AppContainerTypes } from '../inversify/app-container-types';
-import { IAuthService } from '../services/auth0.service';
-
+import { useInjection } from 'inversify-react';
 type ProfileScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
   'Video'
@@ -23,7 +21,7 @@ export const Lovideo: React.FC<Props> = ({navigation}: Props) => {
     const width = Dimensions.get('screen').width;
     const height = Dimensions.get('screen').height;
 
-    const [videoSource, setVideoSource] = useState<string | null>(null);
+    const [videoSource, setVideoSource] = useState<VideoSource | null>(null);
 
     const randomVideoService = useInjection<IRandomVideoService>(AppContainerTypes.IRandomVideoService);
 
--- src/services/random-video.services.ts
+++ src/services/random-video.services.ts
@@ -2,24 +2,29 @@ import { injectable } from 'inversify';
 import 'react-native-get-random-values';
 import 'react-native-url-polyfill/auto';
 
+export interface UriVideoSource {
+    uri: string;
+}
+
+export type VideoSource = NodeRequire | UriVideoSource;
+
 export interface IRandomVideoService {
-  getRandomVideo(): Promise<any>;
+    getRandomVideo(): Promise<VideoSource>;
 }
 
 @injectable()
 export class FakeRandomVideoServiceFromAssets implements IRandomVideoService {
-  private readonly fake_Video = require('../assets/mock-video/SampleVideo_720x480_1mb.mp4');
+    private readonly fake_Video = require('../assets/mock-video/SampleVideo_720x480_1mb.mp4');
 
-  getRandomVideo(): Promise<any> {
-      return Promise.resolve(this.fake_Video);
-  }
+    getRandomVideo(): Promise<VideoSource> {
+        return Promise.resolve(this.fake_Video);
+    }
 }
 
 @injectable()
 export class RandomVideoService implements IRandomVideoService {
-    getRandomVideo(): Promise<any> {
-
-        const source =  {uri: 'https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_640_3MG.mp4' };
+    getRandomVideo(): Promise<VideoSource> {
+        const source = { uri: 'https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_640_3MG.mp4' };
 
         return Promise.resolve(source);
     }
