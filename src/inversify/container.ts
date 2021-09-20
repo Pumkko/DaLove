b51021306c1536e4fe324a4e08f08fe9fb0f2728
Puma <c.coque@hotmail.fr>
2021-09-20 16:41:40 +0200

    rebind IAuthService to fakeAuthService

--- src/inversify/container.ts
+++ src/inversify/container.ts
@@ -1,5 +1,5 @@
 import { Container, interfaces } from 'inversify';
-import { Auth0Service, IAuthService } from '../services/auth0.service';
+import { FakeAuthService, IAuthService } from '../services/auth0.service';
 import { FakeRandomVideoService, IRandomVideoService } from '../services/random-video.services';
 import { AppContainerTypes } from './app-container-types';
 
@@ -8,7 +8,7 @@ type interfaceContainerMethod = () => interfaces.Container;
 
 const AppContainer: interfaceContainerMethod = () => {
     const container = new Container();
-    container.bind<IAuthService>(AppContainerTypes.IAuthService).to(Auth0Service);
+    container.bind<IAuthService>(AppContainerTypes.IAuthService).to(FakeAuthService);
     container.bind<IRandomVideoService>(AppContainerTypes.IRandomVideoService).to(FakeRandomVideoService);
     return container;
 };
