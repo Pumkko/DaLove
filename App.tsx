881941bf12e236a122a3d68718ab6c6ccd7c6c24
Puma <c.coque@hotmail.fr>
2021-11-06 20:48:18 +0100

    start working on notifications to see how they work

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { MainView } from './src/MainView';
 
 import { createStackNavigator } from '@react-navigation/stack';
@@ -11,10 +11,39 @@ import 'reflect-metadata';
 import { UserProfileCreationScreen } from './src/views/screens/user-profile-creation.screen';
 import MemoryVideoScreen from './src/views/screens/memory-video.screen';
 import { UploadMemoryScreen } from './src/views/screens/upload-memory.screen';
+import { Notifications, Registered, RegistrationError } from 'react-native-notifications';
 
 const Stack = createStackNavigator<RootStackParamList>();
 
 const App: React.FC = () => {
+
+
+    useEffect(() => {
+        Notifications.registerRemoteNotifications();
+
+        Notifications.events().registerNotificationOpened((c) => {
+            console.log(c);
+            console.log('opened');
+        });
+        Notifications.events().registerRemoteNotificationsRegistered((event: Registered) => {
+            // TODO: Send the token to my server so it could send back push notifications...
+            console.log('Device Token Received', event.deviceToken);
+        });
+        Notifications.events().registerRemoteNotificationsRegistrationFailed((event: RegistrationError) => {
+            console.error(event);
+        });
+
+        Notifications.events().registerNotificationReceivedForeground((n) => {
+            console.log(n);
+        });
+
+        Notifications.events().registerNotificationReceivedBackground((n) => {
+            console.log(n);
+        });
+    },[]);
+
+
+
     return (
         <Provider container={AppContainer}>
             <NavigationContainer>
